6c90126d69365c5cf776d7c2592eec70a7a00639
ProggerX <x@proggers.ru>
2025-01-19 17:12:11 +0300

    Change output

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,34 @@ module Main where
 
 data Value = Nil | Number Int | Pair Value Value
 
+minPair :: Value -> String
+minPair (Pair a Nil) = show a
+minPair (Pair a b) =
+  concat
+    [ show a
+    , "."
+    , show b
+    ]
+minPair _ = ""
+
+surround :: String -> String
+surround s = "(" ++ s ++ ")"
+
+surround' :: String -> String
+surround' = init . tail
+
 instance Show Value where
   show Nil = "()"
   show (Number n) = show n
+  show (Pair a Nil) = surround $ show a
+  show (Pair a (Pair b c)) =
+    concat
+      [ "("
+      , show a
+      , " "
+      , surround' $ show (Pair b c)
+      , ")"
+      ]
   show (Pair a b) =
     concat
       [ "("
@@ -14,7 +39,17 @@ instance Show Value where
       , ")"
       ]
 
+tests :: [Value]
+tests =
+  [ Pair (Number 1) (Pair (Number 2) (Number 3))
+  , Pair (Pair (Number 1) (Number 2)) (Number 3)
+  , Pair (Pair (Number 1) (Number 2)) (Pair (Number 3) Nil)
+  , Number 5
+  , Pair (Number 5) Nil
+  ]
+
 main :: IO ()
 main = do
   let lst = Pair (Number 1) (Pair (Number 2) (Pair (Number 3) Nil))
   print lst
+  mapM_ print tests
