05db233b99da24623afd4762d3435fc30f054737
ProggerX <x@proggers.ru>
2025-01-19 18:08:49 +0300

    Changed project structure

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,6 @@
 module Main where
 
-data Value = Nil | Number Int | Pair Value Value
-
-surround :: String -> String
-surround s = "(" ++ s ++ ")"
-
-surround' :: String -> String
-surround' = init . tail
-
-instance Show Value where
-  show Nil = "()"
-  show (Number n) = show n
-  show (Pair a Nil) = surround $ show a
-  show (Pair a (Pair b c)) =
-    concat
-      [ "("
-      , show a
-      , " "
-      , surround' $ show (Pair b c)
-      , ")"
-      ]
-  show (Pair a b) =
-    concat
-      [ "("
-      , show a
-      , " . "
-      , show b
-      , ")"
-      ]
+import Lib
 
 tests :: [Value]
 tests =
