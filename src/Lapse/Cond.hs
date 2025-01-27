0e81fa592197657745c67da2b0cbcf076063f540
ProggerX <x@proggers.ru>
2025-01-27 21:10:58 +0300

    Small fix

--- src/Lapse/Cond.hs
+++ src/Lapse/Cond.hs
@@ -5,5 +5,5 @@ import Lapse.Eval (eval)
 
 cond :: Value -> Value
 cond Nil = Nil
-cond (Pair (Pair c (Pair r Nil)) els) = if eval c /= Nil then r else cond els
+cond (Pair (Pair c (Pair r Nil)) els) = if eval c /= Nil then eval r else cond els
 cond _ = undefined
