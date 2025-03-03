6f454e4fe779720255fcdd5abb46cdccbc8d7e75
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-03-03 17:25:31 +0100

    Simplify State code

--- src/Lapse/Scopes.hs
+++ src/Lapse/Scopes.hs
@@ -1,28 +1,30 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE LambdaCase #-}
 
 module Lapse.Scopes where
 
-import Control.Monad.State (get, gets, put)
+import Control.Monad.State (gets, modify)
+-- import Data.Bifunctor (first)
 import Data.Map.Strict ((!?))
 import Data.Map.Strict qualified as Map
 import Lapse.Types (LapseM, Scope, Scopes, Value (..))
 
 newScope :: (Monad m) => LapseM m ()
-newScope = get >>= put . (Map.empty :)
+newScope = modify (Map.empty :)
 
 addScope :: (Monad m) => Scope m -> LapseM m ()
-addScope = (get >>=) . (put .) . (:)
+addScope s = modify (s :)
 
 addScopes :: (Monad m) => Scopes m -> LapseM m ()
-addScopes = (get >>=) . (put .) . (++) . foldr (:) []
+addScopes ss = modify (ss ++)
 
 dropScope :: (Monad m) => LapseM m ()
-dropScope = get >>= put . tail
+dropScope = modify $ drop 1
 
 changeValue :: (Monad m) => String -> Value m -> LapseM m ()
 changeValue k v =
-  get >>= \case
-    (s : ss) -> put (Map.insert k v s : ss)
+  modify \case
+    s : ss -> Map.insert k v s : ss
     _ -> undefined
 
 getValue' :: String -> Scopes m -> Value m
