2f313b9062314f6d76cb20d8e6d5899b773b1e75
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-03-03 17:32:55 +0100

    Simplify tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,57 +1,56 @@
-import Test.Tasty (defaultMain, testGroup)
-import Test.Tasty.HUnit (testCase, (@?=))
-
-import Data.Functor.Identity (Identity)
+import Data.Functor.Identity (Identity, runIdentity)
 import Lapse (evalLapseM, list, numList, runExpression')
 import Lapse.Operators
-import Lapse.Types (Func, LapseM, Value (..))
+import Lapse.Types (LapseM, Value (..))
+import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 type PValue = Value Identity
 
 showTests :: [(PValue, String)]
 showTests =
-  [ (numList [1, 2, 3], "(1 2 3)")
-  , (Pair (Number 1) (Pair (Number 2) (Number 3)), "(1 2 . 3)")
-  , (Pair (Pair (Number 1) (Number 2)) (Number 3), "((1 . 2) . 3)")
-  , (Pair (Pair (Number 1) (Number 2)) (Pair (Number 3) Nil), "((1 . 2) 3)")
-  , (Number 5, "5")
-  , (Pair (Number 5) Nil, "(5)")
+  [ (numList [1, 2, 3], "(1 2 3)"),
+    (Pair (Number 1) (Pair (Number 2) (Number 3)), "(1 2 . 3)"),
+    (Pair (Pair (Number 1) (Number 2)) (Number 3), "((1 . 2) . 3)"),
+    (Pair (Pair (Number 1) (Number 2)) (Pair (Number 3) Nil), "((1 . 2) 3)"),
+    (Number 5, "5"),
+    (Pair (Number 5) Nil, "(5)")
   ]
 
-opTests :: [(LapseM Identity PValue, Identity PValue)]
+opTests :: [(LapseM Identity PValue, PValue)]
 opTests =
-  [ (ladd Nil, pure $ Number 0)
-  , (lmul Nil, pure $ Number 1)
-  , (ladd $ numList [1, 2, 3], pure $ Number 6)
-  , (lsub $ numList [5, 3], pure $ Number 2)
-  , (lmul $ numList [1, 2, 3, 4], pure $ Number 24)
-  , (ldiv $ numList [55, 11], pure $ Number 5)
-  , (lgrt $ numList [7, 2], pure $ Number 1)
-  , (lgrt $ numList [2, 7], pure Nil)
-  , (lgrt $ numList [5, 5], pure Nil)
-  , (llss $ numList [7, 2], pure Nil)
-  , (llss $ numList [2, 7], pure $ Number 1)
-  , (llss $ numList [5, 5], pure Nil)
-  , (leql $ numList [7, 2], pure Nil)
-  , (leql $ numList [2, 7], pure Nil)
-  , (leql $ numList [5, 5], pure $ Number 1)
+  [ (ladd Nil, Number 0),
+    (lmul Nil, Number 1),
+    (ladd $ numList [1, 2, 3], Number 6),
+    (lsub $ numList [5, 3], Number 2),
+    (lmul $ numList [1, 2, 3, 4], Number 24),
+    (ldiv $ numList [55, 11], Number 5),
+    (lgrt $ numList [7, 2], Number 1),
+    (lgrt $ numList [2, 7], Nil),
+    (lgrt $ numList [5, 5], Nil),
+    (llss $ numList [7, 2], Nil),
+    (llss $ numList [2, 7], Number 1),
+    (llss $ numList [5, 5], Nil),
+    (leql $ numList [7, 2], Nil),
+    (leql $ numList [2, 7], Nil),
+    (leql $ numList [5, 5], Number 1)
   ]
 
-condTests :: [(PValue, Identity PValue)]
+condTests :: [(PValue, PValue)]
 condTests =
-  [ (Nil, pure Nil)
-  , (list [list [Number 1, Number 2], list [Number 2, Number 3], list [Number 3, Number 4]], pure $ Number 2)
-  , (list [list [Nil, Number 2], list [Number 2, Number 3], list [Number 3, Number 4]], pure $ Number 3)
-  , (list [list [Nil, Number 2], list [Nil, Number 3], list [Number 3, Number 4]], pure $ Number 4)
-  , (list [list [Nil, Number 2], list [Nil, Number 3], list [Nil, Number 4]], pure Nil)
+  [ (Nil, Nil),
+    (list [list [Number 1, Number 2], list [Number 2, Number 3], list [Number 3, Number 4]], Number 2),
+    (list [list [Nil, Number 2], list [Number 2, Number 3], list [Number 3, Number 4]], Number 3),
+    (list [list [Nil, Number 2], list [Nil, Number 3], list [Number 3, Number 4]], Number 4),
+    (list [list [Nil, Number 2], list [Nil, Number 3], list [Nil, Number 4]], Nil)
   ]
 
-exprTests :: [(String, Identity String)]
+exprTests :: [(String, String)]
 exprTests =
-  [ ("(+ 1 2)", pure "[3]")
-  , ("(let ((a 1)) a)", pure "[1]")
-  , ("(let ((a 1) (b 2) (c 3)) '(,a ,b ,c ,(+ a b c)))", pure "[(1 2 3 6)]")
-  , ("(let ((a \"stra\") (b \"bstr\")) (concat a b))", pure "[\"strabstr\"]")
+  [ ("(+ 1 2)", "[3]"),
+    ("(let ((a 1)) a)", "[1]"),
+    ("(let ((a 1) (b 2) (c 3)) '(,a ,b ,c ,(+ a b c)))", "[(1 2 3 6)]"),
+    ("(let ((a \"stra\") (b \"bstr\")) (concat a b))", "[\"strabstr\"]")
   ]
 
 main :: IO ()
@@ -59,16 +58,20 @@ main =
   defaultMain $
     testGroup
       "all"
-      [ testGroup
-          "show"
-          $ map (\(t, x) -> testCase "test" $ show t @?= x) showTests
-      , testGroup
+      [ testGroup "show" [testCase x $ show t @?= x | (t, x) <- showTests],
+        testGroup
           "operators"
-          $ map (\(t, x) -> testCase "test" $ evalLapseM t @?= x) opTests
-      , testGroup
+          [ testCase (show x) $ runIdentity (evalLapseM t) @?= x
+          | (t, x) <- opTests
+          ],
+        testGroup
           "cond"
-          $ map (\(t, x) -> testCase "test" $ (evalLapseM . cond) t @?= x) condTests
-      , testGroup
+          [ testCase (show x) $ (runIdentity . evalLapseM . cond) t @?= x
+          | (t, x) <- condTests
+          ],
+        testGroup
           "expression tests"
-          $ map (\(t, x) -> testCase "test" $ runExpression' t @?= x) exprTests
+          [ testCase x $ runIdentity (runExpression' t) @?= x
+          | (t, x) <- exprTests
+          ]
       ]
